e86c92fa365fb613d173d2557a240ddcd99b9cb6
juanda <juandacorreo@gmail.com>
2022-05-09 23:40:15 +0200

    test  workflow

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -20,7 +20,7 @@ const Footer = (): JSX.Element => {
   return (
     <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
       <Typography variant="h6" align="center" gutterBottom>
-        ADIDAS Challenge
+        Adidas Challenge
       </Typography>
       <Typography
         variant="subtitle1"
